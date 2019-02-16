a4c4e09f5c7553a0f36c400ffca10cc83efd5d75
Arnab Chanda <arnabchanda964@gmail.com>
2019-02-17 00:21:18 +0530

    Set up the script of Instagram Liker

---
+++ Instagram Liker/InstagramLiker.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from time import sleep
+choice = input("Enter 1 for facebook log in 2 for email login : ")
+email = input("Enter the email : ")
+password = input("Enter the password : ")
+tag = input("Enter the tag you want to search for : ")
+number_of_tags = int(input("Enter the number of tags that you want : "))
+
+url1 = "https://www.instagram.com"
+driver = webdriver.Chrome("C:/Users/Arnab/Downloads/chromedriver")
+driver.set_page_load_timeout(30)
+driver.get(url1)
+if (choice == 1):
+    # statements for facebook login
+    driver.find_element_by_xpath("//section/main/article/div[2]/div[1]/div/form/div[1]/button").click()
+    driver.find_element_by_name("email").send_keys(email)
+    driver.find_element_by_name("pass").send_keys(password)
+    driver.find_element_by_name("login").click()
+elif(choice == 2):
+    # statements for email login
+    driver.find_element_by_xpath("//section/main/article/div[2]/div[2]/p/a").click()
+    driver.find_element_by_name("username").send_keys(email)
+    driver.find_element_by_name("password").send_keys(password)
+    driver.find_element_by_xpath("//section/main/div/article/div/div[1]/div/form/div[3]/button").click()   
+
+
+ListofElements = []
+
+url = "https://www.instagram.com/explore/tags/"+tag+"/"
+print(url)
+driver.get(url)
+#write code for the like part here!
+driver.quit()
