0dd0c9d77b6e167276dbbc3a197ced1fb9febcb8
Arnab Chanda <arnabchanda964@gmail.com>
2019-02-19 00:37:29 +0530

    Like detection enabled

--- Instagram Liker/InstagramLiker.py
+++ Instagram Liker/InstagramLiker.py
@@ -10,16 +10,17 @@ email = input("Enter the email : ")
 password = input("Enter the password : ")
 tags = input("Enter the tag you want to search for : (saperate the tags by a ',')").split(",")
 limit = int(input("Enter how many pictures you want to like?"))
-time = (limit * 5) + 6
+time = (len(tags) * limit * 5) + 6
 print("It will take approximately ",time,"seconds to run!")
 sleep(2)
 driver = webdriver.Chrome(path)
 url1 = "https://www.instagram.com"
-driver.set_page_load_timeout(30)
+driver.set_page_load_timeout(50)
 driver.get(url1)
 if (choice == 1):
     # statements for facebook login
     driver.find_element_by_xpath("//section/main/article/div[2]/div[1]/div/form/div[1]/button").click()
+    sleep(2)
     driver.find_element_by_name("email").send_keys(email)
     driver.find_element_by_name("pass").send_keys(password)
     driver.find_element_by_name("login").click()
@@ -43,13 +44,27 @@ for tag in tags:
     driver.find_element_by_xpath("//section/main/article/div[1]/div/div/div[1]/div[1]").click()
     sleep(1)
     #clicking the first like 
-    driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/article/div[2]/section[1]/span[1]").click()
+    driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/article/div[2]/section[1]/span[1]').click()
     sleep(3)
     driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/div/a").click()
     sleep(2)
-    for i in range(limit-1):
-        driver.find_element_by_xpath("/html/body/div[2]/div[2]/div/article/div[2]/section[1]/span[1]/button/span").click()
-        sleep(3)
+    i = 0
+    while(i < limit-1):
+        btn = driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/article/div[2]/section[1]/span[1]/button/span')
+        print(btn)
+        label = btn.get_attribute("aria-label")
+        print(label)
+        if(label == "Like"):
+            print("Here")
+            i = i+1
+            driver.find_element_by_xpath('/html/body/div[2]/div[2]/div/article/div[2]/section[1]/span[1]/button/span').click()
+            sleep(3)
+        else:
+            print("over here")
+            i = i - 1
+            driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/div/a[2]").click()
+            sleep(2)
+            continue
         driver.find_element_by_xpath("/html/body/div[2]/div[1]/div/div/a[2]").click()
         sleep(2)
 driver.quit()
