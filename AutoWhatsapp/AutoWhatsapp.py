90aa168290bd3ae35e92477cf563f9b4bdafc790
Arnab Chanda <arnabchanda964@gmail.com>
2019-04-30 22:14:12 +0530

    fixed minor bugs of auto whatsapp

--- AutoWhatsapp/AutoWhatsapp.py
+++ AutoWhatsapp/AutoWhatsapp.py
@@ -1,4 +1,5 @@
-    
+import os
+import sys
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -6,13 +7,15 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 
+path = os.path.join(sys.path[0], 'chromedriver.exe')   #Path of the chrome web driver
+
 target = str(input('Name of person:'))
 
 string = str(input('Message: '))
 
 n = int(input('Iteration: '))
 
-driver = webdriver.Chrome('<path>')   #Path of the chrome web driver
+driver = webdriver.Chrome(path) 
  
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
@@ -30,3 +33,5 @@ input_box = wait.until(EC.presence_of_element_located((
 for i in range(n):
     input_box.send_keys(string + Keys.ENTER)
     time.sleep(1)
+
+driver.quit()
